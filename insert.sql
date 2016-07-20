912d053d90f82a2b73b9e59b6a446e238bbccdab
Jean-Philippe Chapdelaine <jean.philippe.chap@gmail.com>
2016-07-20 18:24:37 -0400

    insert pour la BD

---
+++ insert.sql
@@ -0,0 +1,33 @@
+INSERT INTO [dbo].[Roles] ([RoleName])
+VALUES ('admin');
+GO
+INSERT INTO [dbo].[Roles] ([RoleName])
+VALUES ('circle');
+GO
+INSERT INTO [dbo].[Roles] ([RoleName])
+VALUES ('square');
+GO
+
+INSERT INTO [dbo].[Users] ([Email]
+	, [IsLocked]
+	, [PasswordHash]
+	, [PasswordSalt]
+	, [Username])
+VALUES('test@test.com', 0, 'BqPuISzor2MSmmoheGLo5Wr+Tps1VZ2OHfzc3ll0n2Gj4EArJtDWRHm6/vDUOpu9fc2Al91urB5GNbgzabahaw==', 'ca1587f1-4eea-4f41-aad5-4f95bb8f9ae9', 'admin')
+GO
+
+INSERT INTO [dbo].[UserRoles] ([UserId], [RoleId])
+VALUES (1, 1)
+GO
+
+INSERT INTO [dbo].[AdminOptions] ([IsLowerCaseCharacterRequired]
+	, [IsNumberRequired]
+	, [IsSpecialCharacterRequired]
+	, [IsUpperCaseCharacterRequired]
+	, [MaxLoginAttempt]
+	, [MinPasswordLength]
+	, [NumberOfPasswordToKeepInHistory]
+	, [PasswordExpirationDurationInDays]
+	, [TimeoutAfterMaxLoginReachedInMinutes])
+VALUES (0, 0, 0, 0, 10, 10, 3, 90, 1)
+GO
